d8f7ec553ceeafd5a7dc97a49d3c233868dc7f73
Andre Branco <avbranco@air.local>
2011-12-04 08:50:08 -0200

    Add script to setup

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#In your ~/ create symlink for you dot files
+
+ln -nfs ~/Work/dotfiles/emacs.d ~/.emacs.d
+ln -nfs ~/Work/dotfiles/emacs.d/.emacs ~/.emacs
+
+ln -nfs ~/Work/dotfiles/vimfiles/ ~/.vim
+ln -nfs ~/Work/dotfiles/vimrc ~/.vimrc
+ln -nfs ~/Work/dotfiles/gvimrc ~/.gvimrc
+
+ln -nfs ~/Work/dotfiles/ruby/pryrc ~/.pryrc
+ln -nfs ~/Work/dotfiles/ruby/autotest ~/.autotest
+
+
+ln -nfs ~/Work/dotfiles/git/gitconfig ~/.gitconfig
+ln -nfs ~/Work/dotfiles/git/gitignore_global ~/.gitignore_global
+
+ln -nfs ~/Work/dotfiles/ssh/config ~/.ssh/config
+
+ln -nfs Work/dotfiles/bash_profile ~/.bash_profile
+ln -nfs Work/dotfiles/bashrc ~/.bashrc
+
+
