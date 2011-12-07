68153276b85eeb4ebf1286f6527bd28938998400
Andre Branco <avbranco@air.local>
2011-12-07 19:49:45 -0200

    Change the case of work folder

--- setup.sh
+++ setup.sh
@@ -1,22 +1,20 @@
 #In your ~/ create symlink for you dot files
 
-ln -nfs ~/Work/dotfiles/emacs.d ~/.emacs.d
-ln -nfs ~/Work/dotfiles/emacs.d/.emacs ~/.emacs
+ln -nfs ~/work/dotfiles/emacs.d ~/.emacs.d
+ln -nfs ~/work/dotfiles/emacs.d/.emacs ~/.emacs
 
-ln -nfs ~/Work/dotfiles/vimfiles/ ~/.vim
-ln -nfs ~/Work/dotfiles/vimrc ~/.vimrc
-ln -nfs ~/Work/dotfiles/gvimrc ~/.gvimrc
+ln -nfs ~/work/dotfiles/vimfiles/ ~/.vim
+ln -nfs ~/work/dotfiles/vimrc ~/.vimrc
+ln -nfs ~/work/dotfiles/gvimrc ~/.gvimrc
 
-ln -nfs ~/Work/dotfiles/ruby/pryrc ~/.pryrc
-ln -nfs ~/Work/dotfiles/ruby/autotest ~/.autotest
-
-
-ln -nfs ~/Work/dotfiles/git/gitconfig ~/.gitconfig
-ln -nfs ~/Work/dotfiles/git/gitignore_global ~/.gitignore_global
-
-ln -nfs ~/Work/dotfiles/ssh/config ~/.ssh/config
-
-ln -nfs Work/dotfiles/bash_profile ~/.bash_profile
-ln -nfs Work/dotfiles/bashrc ~/.bashrc
+ln -nfs ~/work/dotfiles/ruby/pryrc ~/.pryrc
+ln -nfs ~/work/dotfiles/ruby/autotest ~/.autotest
 
 
+ln -nfs ~/work/dotfiles/git/gitconfig ~/.gitconfig
+ln -nfs ~/work/dotfiles/git/gitignore_global ~/.gitignore_global
+
+ln -nfs ~/work/dotfiles/ssh/config ~/.ssh/config
+
+ln -nfs ~/work/dotfiles/bash_profile ~/.bash_profile
+ln -nfs ~/work/dotfiles/bashrc ~/.bashrc
