da18ec0dce7b5cf965c6ea3f484eb4268feb943a
Andre Branco <avbranco@gmail.com>
2012-04-14 18:52:18 -0300

    Ajust some tmux conf

--- scripts/rp-code.sh
+++ scripts/rp-code.sh
@@ -3,14 +3,13 @@ set -e
 
 tmux new-session -d -s rp-code
 
-
 tmux new-window -t rp-code:1 -n 'server'
-tmux new-window -t rp-code:2 -n 'rps'
-tmux new-window -t rp-code:3 -n 'scheduler'
+#tmux new-window -t rp-code:2 -n 'rps'
+#tmux new-window -t rp-code:3 -n 'scheduler'
 
 tmux send-keys -t rp-code:1 "gorp" C-m
-tmux send-keys -t rp-code:2 "gorps" C-m
-tmux send-keys -t rp-code:3 "gorp" C-m
+#tmux send-keys -t rp-code:2 "gorps" C-m
+#tmux send-keys -t rp-code:3 "gorp" C-m
 tmux send-keys -t rp-code:0 "gorp" C-m
 
 tmux rename-window -t rp-code:0 'vim'
