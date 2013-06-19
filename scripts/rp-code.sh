5d26e5308c3430a03096ee5a51da9ddd84b08610
Andre Branco <avbranco@gmail.com>
2013-06-19 10:15:07 -0300

    Updates tmux script start to use zeus

--- scripts/rp-code.sh
+++ scripts/rp-code.sh
@@ -5,10 +5,13 @@ tmux new-session -d -s rp-code
 
 tmux new-window -t rp-code:2 -n 'console'
 tmux new-window -t rp-code:3 -n 'server'
+tmux new-window -t rp-code:4 -n 'zeus'
+
+tmux send-keys -t rp-code:4 "gorp; zeus start" C-m
 
 tmux send-keys -t rp-code:1 "gorp; vim" C-m
 tmux send-keys -t rp-code:2 "gorp; rc" C-m
-tmux send-keys -t rp-code:3 "gorp; rails s" C-m
+tmux send-keys -t rp-code:3 "gorp; zeus server" C-m
 
 tmux rename-window -t rp-code:1 'vim'
 tmux select-window -t rp-code:1
