3764c9e762c5c78fc34d6b50d373021ab3626b5e
Andre Branco <avbranco@gmail.com>
2014-06-30 17:54:34 -0300

    Adjust workflow

--- scripts/pia-code.sh
+++ scripts/pia-code.sh
@@ -3,24 +3,24 @@ set -e
 
 cd ~/work/pia
 
-tmux new-session -d -s pia-code
-tmux new-session -d -s pia-monitor
+tmux new-session -d -s code
+tmux new-session -d -s monitor
 
 #code
-tmux new-window -t pia-code:2 -n 'console'
-tmux new-window -t pia-code:3 -n 'server'
+tmux new-window -t code:2 -n 'console'
+tmux new-window -t code:3 -n 'server'
 
-tmux send-keys -t pia-code:1 "vim" C-m
-tmux send-keys -t pia-code:2 "rc" C-m
-tmux send-keys -t pia-code:3 "rs" C-m
+tmux send-keys -t code:1 "vim" C-m
+tmux send-keys -t code:2 "rc" C-m
+tmux send-keys -t code:3 "rs" C-m
 
-tmux rename-window -t pia-code:1 'vim'
-tmux select-window -t pia-code:1
+tmux rename-window -t code:1 'vim'
+tmux select-window -t code:1
 
 #monitor
-tmux new-window -t pia-monitor:2 -n 'zeus'
-tmux send-keys -t pia-monitor:2 "zeus start" C-m
-tmux send-keys -t pia-monitor:1 "ssh mcd5 'pia_alert_watcher.rb'" C-m
+tmux new-window -t monitor:2 -n 'zeus'
+tmux send-keys -t monitor:2 "zeus start" C-m
+tmux send-keys -t monitor:1 "ssh mcd5 'pia_alert_watcher.rb'" C-m
 
-tmux -2 attach-session -t pia-code
+tmux -2 attach-session -t code
 
